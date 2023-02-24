1e5a90ab56ac278f76458f614c30f5943327de0e
qpwlkq <qpwlkq@qq.com>
2023-02-25 01:55:02 +0800

    first PR

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	fmt.Println("a = ", a)
+	fmt.Print("wakabak")
+	var b int = 100
+
+	fmt.Println(b)
+	var c = 100
+	fmt.Println(c)
+}
