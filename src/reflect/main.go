835c7a473d0b060260bead107e03d9672ae800f2
winston.qin <winston.qin@temu.com>
2023-05-14 12:11:56 +0800

    t

---
+++ src/reflect/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+
+	name = "xishanyu"
+
+	var allType interface{}
+	allType = name
+
+	value, _ := allType.(string)
+	fmt.Println(value)
+
+}
